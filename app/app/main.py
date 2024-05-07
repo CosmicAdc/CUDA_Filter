9e67f80486eded67fdccd639cd7cd00c44f24a02
Cosmic <andresbloc@hotmail.es>
2024-05-07 15:47:54 -0500

    Servicio para subir imagen a ESCALA DE GRISES HECHO

--- app/app/main.py
+++ app/app/main.py
@@ -1,6 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from typing import List
+import cv2
+import uuid
+import numpy as np
+
 
 app = FastAPI()
+
+SAVE_PATH_OR = "app/static/originales/"
+
+##Subir imagen al servidor
+@app.post("/upload/")
+async def upload_files(files: List[UploadFile] = File(...)):
+    for file in files:
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # Leer la imagen en color
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convertir a blanco y negro
+        
+        image_id = str(uuid.uuid4())
+
+        cv2.imwrite(f"{SAVE_PATH_OR}ORIGINAL_{image_id}.jpg", gray_img)
+
+        print(f"File '{file.filename}' guardado con id: {image_id}")
+    return {"message": "Subida exitosamente"}
+
+
 
 @app.get("/")
 def read_root():
